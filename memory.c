b3e1e85a1f701ab28e877c72dac2606039f2dc07
vetsasp <vetsasp@gmail.com>
2025-01-16 18:37:50 -0500

    Began work on methods. Methods can now be compiled.

--- memory.c
+++ memory.c
@@ -41,6 +41,8 @@ static void freeObject(Obj* object) {
 
   switch (object->type) {
     case OBJ_CLASS: {
+      ObjClass* klass = (ObjClass*)object;
+      freeTable(&klass->methods);
       FREE(ObjClass, object);
       break;
     }
@@ -141,6 +143,7 @@ static void blackenObject(Obj* object) {
     case OBJ_CLASS: {
       ObjClass* klass = (ObjClass*)object;
       markObject((Obj*)klass->name);
+      markTable(&klass->methods);
       break;
     }
     case OBJ_CLOSURE: {
