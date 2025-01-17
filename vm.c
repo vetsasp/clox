ad2a7be2b2813fed999887e6a6c14f8038ff819f
vetsasp <vetsasp@gmail.com>
2025-01-17 18:14:14 -0500

    Ch 29 complete. Superclasses fully implemented. Lox fully implemented, actually. Final chapter is only opimization.

--- vm.c
+++ vm.c
@@ -387,6 +387,15 @@ static InterpretResult run() {
         push(value);
         break;
       }
+      case OP_GET_SUPER: {
+        ObjString* name = READ_STRING();
+        ObjClass* superclass = AS_CLASS(pop());
+
+        if (!bindMethod(superclass, name)) {
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break; 
+      }
       case OP_EQUAL: {
         Value b = pop();
         Value a = pop();
@@ -455,6 +464,17 @@ static InterpretResult run() {
         ObjString* method = READ_STRING();
         int argCount = READ_BYTE();
         if (!invoke(method, argCount)) {
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        frame = &vm.frames[vm.frameCount - 1];
+        break;
+      }
+      case OP_SUPER_INVOKE: {
+        ObjString* method = READ_STRING();
+        int argCount = READ_BYTE();
+        ObjClass* superclass = AS_CLASS(pop());
+
+        if (!invokeFromClass(superclass, method, argCount)) {
           return INTERPRET_RUNTIME_ERROR;
         }
         frame = &vm.frames[vm.frameCount - 1];
